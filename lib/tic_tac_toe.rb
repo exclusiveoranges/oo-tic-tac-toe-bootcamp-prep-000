8a9aa49bc061058bd48f29449065dbbc146ab2d7
Ryan O'Toole <otoolerj@gmail.com>
2018-03-19 19:23:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,8 +88,8 @@ def position_taken?(index)
   @board[index] != " " && @board[index] != "" && @board[index] != nil
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
 end
 
 def turn(board)
